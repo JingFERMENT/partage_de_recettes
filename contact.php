a202a6b42c9129e4c0de2de50ecf6ed8f051eb20
Jing ZHANG <jing.zhang.ferment@gmail.com>
2024-06-18 15:36:07 +0200

    secure form in php

--- contact.php
+++ contact.php
@@ -16,11 +16,10 @@
         <!-- header -->
         <?php require_once(__DIR__ . '/header.php'); ?>
         <h1>Contactez nous</h1>
-        <form>
+        <form action="submit_contact.php" method="GET">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
-                <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
             </div>
             <div class="mb-3">
                 <label for="message" class="form-label">Votre message</label>
